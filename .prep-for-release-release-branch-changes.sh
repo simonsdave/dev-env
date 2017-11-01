e6886d1265190e4875c080abbb00492dcf4dd71d
simonsdave <simonsdave@gmail.com>
2017-11-01 18:00:49 -0500

    simplify .prep-for-release-release-branch-changes.sh

--- .prep-for-release-release-branch-changes.sh
+++ .prep-for-release-release-branch-changes.sh
@@ -11,14 +11,7 @@ fi
 
 RELEASE_BRANCH=${1:-}
 
-search_and_replace() {
-    if ! git diff --quiet "${2:-}"; then exit 1; fi
-    sed -i -e "${1:-}" "${2:-}"
-    if git diff --quiet "${2:-}"; then exit 2; fi
-    return 0
-}
-
-search_and_replace \
+sed -i -e \
     "s|\\?branch=master|?branch=$RELEASE_BRANCH|g" \
     "$SCRIPT_DIR_NAME/README.md"
 
