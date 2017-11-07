6d562a1e91581eb61c4c35b223518c60b5ca0769
simonsdave <simonsdave@gmail.com>
2017-11-07 18:51:38 -0500

    simplify prep-for-release-python.sh

--- bin/prep-for-release-python.sh
+++ bin/prep-for-release-python.sh
@@ -1,60 +1,17 @@
 #!/usr/bin/env bash
 
 #
-# prep-for-release-python.sh is a pythin specific wrapper around
+# prep-for-release-python.sh is a python specific wrapper around
 # the general purpose prep-for-release.sh. To run prep-for-release.sh
 # you need the release version number. For Python projects this
-# version number can be extracted from the project __init__.py file.
+# version number can be extracted from the project's __init__.py
+# file.
 #
 
 set -e
 
-echo_if_verbose() {
-    if [ "1" -eq "${VERBOSE:-0}" ]; then
-        echo "${1:-}"
-    fi
-    return 0
-}
-
-confirm_ok_to_proceed() {
-    if [ "0" -eq "${QUIET:-0}" ]; then
-        while true
-        do
-            read -p "${1:-} (y/n)> " -n 1 -r
-            echo
-
-            case "${REPLY,,}" in
-                y)
-                    break
-                    ;;
-                n)
-                    exit 0
-                    ;;
-                *)
-                    ;;
-            esac
-        done
-    fi
-    return 0
-}
-
-VERBOSE=0
-
-while true
-do
-    case "${1,,}" in
-        --verbose)
-            shift
-            VERBOSE=1
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-
 if [ $# != 0 ]; then
-    echo "usage: $(basename "$0") [--verbose]" >&2
+    echo "usage: $(basename "$0")" >&2
     exit 1
 fi
 
