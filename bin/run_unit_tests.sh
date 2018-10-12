d998504be803292a17e7414809f2f24046f9fe47
Dave Simons <simonsdave@gmail.com>
2018-10-12 00:02:47 -0400

    coveralls and run_unit_tests.sh fix

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -7,9 +7,12 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
+docker run --rm --volume "$DEV_ENV_SOURCE_CODE:/app" "$DEV_ENV_DOCKER_IMAGE" run_unit_tests.sh "$DEV_ENV_PACKAGE"
+
 USER=$(stat -c "%u" "$0")
 GROUP=$(stat -c "%g" "$0")
+chown "$USER.$GROUP" "$DEV_ENV_SOURCE_CODE/.coverage"
 
-docker run --rm --volume "$DEV_ENV_SOURCE_CODE:/app" "$DEV_ENV_DOCKER_IMAGE" run_unit_tests.sh "$USER" "$GROUP" "$DEV_ENV_PACKAGE"
+sed -i -e "s|/app/|$DEV_ENV_SOURCE_CODE/|g" "$DEV_ENV_SOURCE_CODE/.coverage"
 
 exit 0
--- ubuntu/xenial/run_unit_tests.sh
+++ ubuntu/xenial/run_unit_tests.sh
@@ -2,21 +2,17 @@
 
 set -e
 
-if [ $# != 3 ]; then
-    echo "usage: $(basename "$0") <user> <group> <package>" >&2
+if [ $# != 1 ]; then
+    echo "usage: $(basename "$0") <package>" >&2
     exit 1
 fi
 
-USER=${1:-}
-GROUP=${2:-}
-PACKAGE=${3:-}
+PACKAGE=${1:-}
 
 nosetests \
     --with-coverage \
     --cover-erase \
     --cover-branches \
     "--cover-package=$PACKAGE"
-
-chown "$USER.$GROUP" /app/.coverage
 
 exit 0
