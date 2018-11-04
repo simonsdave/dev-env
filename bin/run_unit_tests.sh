f0b28e3dac7ac0da25dfc81771d908fb58eb1458
simonsdave <simonsdave@gmail.com>
2018-11-04 08:04:35 -0500

    fix test dir arg parsing in bin/run_unit_tests.sh

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -10,14 +10,12 @@ fi
 USER=$(stat -c "%u" "$0")
 GROUP=$(stat -c "%g" "$0")
 
-DIRS=$*
-
 docker run \
     --rm \
     --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     --volume "$DEV_ENV_SOURCE_CODE:/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$USER" "$GROUP" "${DIRS[@]}"
+    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$USER" "$GROUP" "$@"
 
 sed -i -e "s|/app/|$DEV_ENV_SOURCE_CODE/|g" "$DEV_ENV_SOURCE_CODE/.coverage"
 
