20395cb429301de9fb7f76a0822259a28463c820
simonsdave <simonsdave@gmail.com>
2019-01-15 23:31:29 -0500

    more get run_unit_tests.sh working again on travis

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -16,15 +16,16 @@ do
     esac
 done
 
-USER=$(stat -c "%u" "$0")
-GROUP=$(stat -c "%g" "$0")
+rm -f "$DEV_ENV_SOURCE_CODE/.coverage"
+touch "$DEV_ENV_SOURCE_CODE/.coverage"
+chmod a+wr "$DEV_ENV_SOURCE_CODE/.coverage"
 
 docker run \
     --rm \
     --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     --volume "$DEV_ENV_SOURCE_CODE:/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$USER" "$GROUP" "$@"
+    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$@"
 
 if [ -e "$DEV_ENV_SOURCE_CODE/.coverage" ]; then
     sed -i -e "s|/app/|$DEV_ENV_SOURCE_CODE/|g" "$DEV_ENV_SOURCE_CODE/.coverage"
--- ubuntu/xenial/run_unit_tests.sh
+++ ubuntu/xenial/run_unit_tests.sh
@@ -2,29 +2,18 @@
 
 set -e
 
-if [ $# -lt 3 ]; then
-    echo "usage: $(basename "$0") <package> <user> <group> [<dir1> <dir2> ... <dirN>]" >&2
+if [ $# -lt 1 ]; then
+    echo "usage: $(basename "$0") <package> [<dir1> <dir2> ... <dirN>]" >&2
     exit 1
 fi
 
 PACKAGE=${1:-}
 shift
 
-USER=${1:-}
-shift
-
-GROUP=${1:-}
-shift
-
 nosetests \
     --with-coverage \
-    --cover-erase \
     --cover-branches \
     "--cover-package=$PACKAGE" \
     "$@"
-
-if [ -e "/app/.coverage" ]; then
-    chown "$USER.$GROUP" "/app/.coverage"
-fi
 
 exit 0
