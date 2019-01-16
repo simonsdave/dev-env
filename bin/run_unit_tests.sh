b6cc6b4eef11591dda7505f07ab57b26abbd3627
simonsdave <simonsdave@gmail.com>
2019-01-15 23:59:21 -0500

    more get run_unit_tests.sh working again on travis

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -16,8 +16,7 @@ do
     esac
 done
 
-rm -f "$DEV_ENV_SOURCE_CODE/.coverage"
-touch "$DEV_ENV_SOURCE_CODE/.coverage"
+echo "!coverage.py: This is a private format, don't read it directly!{}" > "$DEV_ENV_SOURCE_CODE/.coverage"
 chmod a+wr "$DEV_ENV_SOURCE_CODE/.coverage"
 
 docker run \
@@ -25,7 +24,7 @@ docker run \
     --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     --volume "$DEV_ENV_SOURCE_CODE:/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    run_unit_tests.sh "$DEV_ENV_PACKAGE" "$@"
+    nosetests --with-coverage --cover-branches "--cover-package=$DEV_ENV_PACKAGE" "$@"
 
 if [ -e "$DEV_ENV_SOURCE_CODE/.coverage" ]; then
     sed -i -e "s|/app/|$DEV_ENV_SOURCE_CODE/|g" "$DEV_ENV_SOURCE_CODE/.coverage"
