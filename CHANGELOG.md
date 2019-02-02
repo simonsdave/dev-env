7ec27e6e461fe79f9ae02b10efa9004d201d5393
Dave Simons <simonsdave@gmail.com>
2019-02-02 15:41:10 -0500

    added build_python_package.sh

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ This project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Added
 
-- Nothing
+- added ```build_python_package.sh```
 
 ### Changed
 
---
+++ bin/build_python_package.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# https://docs.python.org/2/distutils/sourcedist.html
+
+set -e
+
+usage() {
+    echo "usage: $(basename "$0")" >&2
+}
+
+while true
+do
+    case "${1:-}" in
+        --help)
+            shift
+            usage
+            exit 1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ $# != 0 ]; then
+    usage
+    exit 1
+fi
+
+docker run \
+    --rm \
+    --volume "$DEV_ENV_SOURCE_CODE:/app" \
+    "$DEV_ENV_DOCKER_IMAGE" \
+    python setup.py bdist_wheel sdist --formats=gztar
+
+ls -la "$DEV_ENV_SOURCE_CODE/dist"
+
+exit 0
