20fafa4fab18085826c26d7e1542c5d44ce1efe2
Dave Simons <simonsdave@gmail.com>
2019-02-03 13:23:05 -0500

    add bdist_wheel to build_python_package.sh

--- bin/build_python_package.sh
+++ bin/build_python_package.sh
@@ -27,13 +27,23 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
+#
+# :TRICKY: The implementation below feels more complicated than it
+# should be but was arrived at to deal with permissioning instability/inconsistency
+# when the simple/obvious thing to do would be to have the "python setup.py"
+# run inside the container generating packages directly into a
+# directory on the host mapped into the container using --volume.
+# The solution does all the generation of packages inside the container
+# and then manually pulls the packages out of the container before
+# manually deleting the container. It's a little awkward but it's reliable.
+#
 DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
 
 docker run \
     --name "$DOCKER_CONTAINER_NAME" \
     --volume "$DEV_ENV_SOURCE_CODE:/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    /bin/bash -c 'cp -r /app ~; cd ~/app; python setup.py sdist --formats=gztar'
+    /bin/bash -c 'cp -r /app ~; cd ~/app; python setup.py bdist_wheel sdist --formats=gztar'
 
 DIST_DIR_IN_CONTAINER=$(docker run --rm "$DEV_ENV_DOCKER_IMAGE" /bin/bash -c 'echo ~')/app/dist
 rm -rf "$DEV_ENV_SOURCE_CODE/dist"
