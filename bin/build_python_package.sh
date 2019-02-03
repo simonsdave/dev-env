183575142af745fb436a733ecf4f3c8f88cd3001
Dave Simons <simonsdave@gmail.com>
2019-02-03 13:03:04 -0500

    refactor build_python_package.sh

--- bin/build_python_package.sh
+++ bin/build_python_package.sh
@@ -27,26 +27,18 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
-set -x
 DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
 
 docker run \
     --name "$DOCKER_CONTAINER_NAME" \
     --volume "$DEV_ENV_SOURCE_CODE:/app" \
-    --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    python setup.py sdist --formats=gztar
+    /bin/bash -c 'cp -r /app ~; cd ~/app; python setup.py sdist --formats=gztar'
 
-# /bin/bash -c 'mkdir ~/newapp; cp -r /app ~/newapp/.; cd ~/newapp/app; python setup.py sdist --formats=gztar; echo "--------------------"; echo ~; ls -la ~/newapp/app/dist; echo "--------------------"'
+DIST_DIR_IN_CONTAINER=$(docker run --rm "$DEV_ENV_DOCKER_IMAGE" /bin/bash -c 'echo ~')/app/dist
+rm -rf "$DEV_ENV_SOURCE_CODE/dist"
+docker container cp "$DOCKER_CONTAINER_NAME:$DIST_DIR_IN_CONTAINER" "$DEV_ENV_SOURCE_CODE"
 
-# rm -rf "$DEV_ENV_SOURCE_CODE/dist"
-# mkdir "$DEV_ENV_SOURCE_CODE/dist"
-# docker cp "$DOCKER_CONTAINER_NAME:/newapp/app/dist/*" "$DEV_ENV_SOURCE_CODE/dist/."
-
-docker rm "$DOCKER_CONTAINER_NAME"
-
-ls -la "$DEV_ENV_SOURCE_CODE/dist"
-
-set +x
+docker container rm "$DOCKER_CONTAINER_NAME"
 
 exit 0
