b3d76b6ba7552ce31010d8458798ae2f9e74f210
Dave Simons <simonsdave@gmail.com>
2019-03-18 22:48:32 -0400

    fix: build-docker-image.sh Dockerfile dir bug

--- ubuntu/xenial/build-docker-image.sh
+++ ubuntu/xenial/build-docker-image.sh
@@ -14,6 +14,17 @@ TAG=${2:-}
 
 CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
 
+#
+# Dockerfile copied to context directory because some
+# CI builds were generating the error
+#
+#   Dockerfile must be within the build context
+#
+# This is is a function of the docker version but still
+# need to deal with the reality.
+#
+cp "$SCRIPT_DIR_NAME/Dockerfile" "$CONTEXT_DIR/."
+
 pushd "$(git rev-parse --show-toplevel)/bin/in_container"
 tar zcvf "$CONTEXT_DIR/in_container_sh_scripts.tar.gz" ./*.sh
 popd
@@ -22,6 +33,6 @@ cp "$SCRIPT_DIR_NAME/requirements.txt" "$CONTEXT_DIR/."
 
 IMAGE_NAME="$USERNAME/xenial-dev-env:$TAG"
 
-docker build -t "$IMAGE_NAME" --file "$SCRIPT_DIR_NAME/Dockerfile" "$CONTEXT_DIR"
+docker build -t "$IMAGE_NAME" --file "$CONTEXT_DIR/Dockerfile" "$CONTEXT_DIR"
 
 exit 0
