f71822090a1ffeb2565316b32ebfccb761553544
Dave Simons <simonsdave@gmail.com>
2019-03-19 06:14:18 -0400

    docs: fix spelling error in script comment

--- bin/in_container/current-python-version.sh
+++ bin/in_container/current-python-version.sh
@@ -3,7 +3,7 @@
 #
 # In a typical python project you would expect to find an __init__.py
 # containing the version number for the project. The version number is
-# expected to appear on a single line in the __init.py looking something
+# expected to appear on a single line in the __init__.py looking something
 # like:
 #
 #   __version__ = '1.2.0'
@@ -17,8 +17,8 @@
 #
 # -- if the repo is called de-mo, the __init__.py containing the version
 #    number will be found in de_mo/__init__.py relative to the top of
-#    the project's repo - note that - was transformed by a _ which is
-#    done for all _'s
+#    the project's repo - note that "-" was transformed to a "_" which is
+#    done for all underscores
 #
 # -- the PWD is somewhere in the git repo - this assumption can be
 #    overridden using the -d command line option.
