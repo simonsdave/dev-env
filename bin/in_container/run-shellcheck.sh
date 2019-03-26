12e442de34b3ff878786d4a4e2caeaa01e698549
Dave Simons <simonsdave@gmail.com>
2019-03-25 22:16:37 -0400

    feat: add in_container/run-shellcheck.sh

---
+++ bin/in_container/run-shellcheck.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+VERBOSE=0
+
+while true
+do
+    case "$(echo "${1:-}" | tr "[:upper:]" "[:lower:]")" in
+        -v)
+            shift
+            VERBOSE=1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0") [-v]" >&2
+    exit 1
+fi
+
+find "$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")" -name \*.sh | grep -v ./env | while IFS='' read -r FILENAME
+do
+    if [ "1" -eq "${VERBOSE:-0}" ]; then
+        echo "$FILENAME"
+    fi
+
+    shellcheck "$FILENAME"
+done
+
+exit 0
