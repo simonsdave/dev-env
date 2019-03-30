629b8bf2f3e34bcd8071fb7dfcd699b7893c740f
Dave Simons <simonsdave@gmail.com>
2019-03-30 15:59:49 -0400

    refactor: build-readme-dot-rst.sh

--- bin/build-readme-dot-rst.sh
+++ bin/build-readme-dot-rst.sh
@@ -12,12 +12,12 @@ REPO_ROOT_DIR=$(repo-root-dir.sh)
 #
 # :TRICKY: The implementation below feels more complicated than it
 # should be but was arrived at to deal with permissioning instability/inconsistency
-# when the simple/obvious thing to do would be to have the "python /app/README.md /app/README.rst"
+# when the simple/obvious thing to do would be to have the "pandoc README.md README.rst"
 # run inside the container generating README.rst directly into a
 # directory on the host mapped into the container using --volume.
 # The solution does all the generation of README.rst inside the container
-# and then manually pulls the README.rst out of the container before
-# manually deleting the container. It's a little awkward but it's reliable.
+# and then manually pulls it out of the container before manually deleting
+# the container. It's a little awkward but it's reliable.
 #
 DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
 
@@ -25,11 +25,10 @@ docker run \
     --name "$DOCKER_CONTAINER_NAME" \
     --volume "$REPO_ROOT_DIR:/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    /bin/bash -c 'pandoc /app/README.md -o ~/README.rst'
+    pandoc "/app/README.md" -o "/tmp/README.rst"
 
-HOME_DIR_IN_CONTAINER=$(docker run --rm "$DEV_ENV_DOCKER_IMAGE" /bin/bash -c 'echo ~')
 rm -f "$REPO_ROOT_DIR/README.rst"
-docker container cp "$DOCKER_CONTAINER_NAME:$HOME_DIR_IN_CONTAINER/README.rst" "$REPO_ROOT_DIR"
+docker container cp "$DOCKER_CONTAINER_NAME:/tmp/README.rst" "$REPO_ROOT_DIR/README.rst"
 
 docker container rm "$DOCKER_CONTAINER_NAME"
 
