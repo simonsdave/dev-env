d715c3cf794b9341e15b4fdca057f38523b389f2
Dave Simons <simonsdave@gmail.com>
2019-04-03 20:04:12 -0400

    fix: rm on-host run-snyk.sh assump on in-container

--- bin/run-snyk.sh
+++ bin/run-snyk.sh
@@ -15,6 +15,6 @@ docker run \
     --rm \
     --volume "$("$SCRIPT_DIR_NAME/repo-root-dir.sh"):/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    run-snyk.sh "$SNYK_TOKEN"
+    /bin/bash -c "cd /app && run-snyk.sh '$SNYK_TOKEN'"
 
 exit 0
