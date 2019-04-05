0624eae2e354179af092fa3086c4fe4d5a6573bc
Dave Simons <simonsdave@gmail.com>
2019-04-05 07:16:01 -0400

    perf: on-host run-shellcheck.sh optimization

--- bin/run-shellcheck.sh
+++ bin/run-shellcheck.sh
@@ -4,34 +4,15 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-VERBOSE=0
-
-while true
-do
-    case "$(echo "${1:-}" | tr "[:upper:]" "[:lower:]")" in
-        -v)
-            shift
-            VERBOSE=1
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-
 if [ $# != 0 ]; then
-    echo "usage: $(basename "$0") [-v]" >&2
+    echo "usage: $(basename "$0")" >&2
     exit 1
 fi
 
-find "$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")" -name \*.sh | grep -v ./env | while IFS='' read -r FILENAME
-do
-    if [ "1" -eq "${VERBOSE:-0}" ]; then
-        echo "$FILENAME"
-    fi
-    MNT_DIR=$(dirname "$FILENAME")
-    MNT_FILE=$(basename "$FILENAME")
-    docker run --rm -v "$MNT_DIR:/mnt" koalaman/shellcheck:latest "$MNT_FILE"
-done
+docker run \
+    --rm \
+    --volume "$("$SCRIPT_DIR_NAME/repo-root-dir.sh"):/app" \
+    "$DEV_ENV_DOCKER_IMAGE" \
+    /bin/bash -c 'for SHELL_SCRIPT in $(find /app -name \*.sh | egrep -v "^/app/(build|env)"); do echo "$SHELL_SCRIPT" && shellcheck "$SHELL_SCRIPT"; done'
 
 exit 0
