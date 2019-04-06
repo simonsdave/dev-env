8b949498689a993a166015f605712753b1e40ad7
Dave Simons <simonsdave@gmail.com>
2019-04-06 16:42:03 -0400

    debug: why run-snyk.sh fails in CircleCI?

--- bin/run-snyk.sh
+++ bin/run-snyk.sh
@@ -29,10 +29,12 @@ fi
 
 SNYK_TOKEN=${1:-}
 
+set -x
 docker run \
     --rm \
     --volume "$("$SCRIPT_DIR_NAME/repo-root-dir.sh"):/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
     /bin/bash -c "if [ $PIP_INSTALL == 1 ]; then cd /app && pwd && ls -la && pip install -r requirements.txt; fi && snyk auth '$SNYK_TOKEN' && snyk test /app"
+set -x
 
 exit 0
