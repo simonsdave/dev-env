c81702fec7c49538c839de84e570a034f84636b7
Dave Simons <simonsdave@gmail.com>
2019-04-06 13:14:02 -0400

    fix: get run-snyk.sh working for dev-env itself

--- bin/run-snyk.sh
+++ bin/run-snyk.sh
@@ -4,8 +4,26 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
+PIP_INSTALL=0
+
+while true
+do
+    case "$(echo "${1:-}" | tr "[:upper:]" "[:lower:]")" in
+        #
+        # the -p option is only expected to be used by the dev-env project itself
+        #
+        -p)
+            shift
+            PIP_INSTALL=1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
 if [ $# != 1 ]; then
-    echo "usage: $(basename "$0") <synk-token>" >&2
+    echo "usage: $(basename "$0") [-p] <synk-token>" >&2
     exit 1
 fi
 
@@ -15,6 +33,6 @@ docker run \
     --rm \
     --volume "$("$SCRIPT_DIR_NAME/repo-root-dir.sh"):/app" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    /bin/bash -c "snyk auth '$SNYK_TOKEN' && snyk test /app"
+    /bin/bash -c "if [ $PIP_INSTALL == 1 ]; then pushd /app && pip install -r requirements.txt && popd; fi && snyk auth '$SNYK_TOKEN' && snyk test /app"
 
 exit 0
