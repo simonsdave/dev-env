7a2c37fb14a4d30c0e5c35ef3b6b0f07cdd5997c
Dave Simons <simonsdave@gmail.com>
2019-04-07 16:55:16 -0400

    fix: make run-unit-tests.sh work on CircleCI

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -9,26 +9,28 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
+REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
+DOT_COVERAGE=$REPO_ROOT_DIR/.coverage
+
+echo "!coverage.py: This is a private format, don't read it directly!{}" > "$DOT_COVERAGE"
+chmod a+wr "$DOT_COVERAGE"
+
 DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
 
 DUMMY_DOCKER_CONTAINER_NAME=$("${SCRIPT_DIR_NAME}/create-dummy-docker-container.sh")
 
+# sed is run inside the container because that means we con control
+# the OS and thus sed version
 docker run \
     --name "$DOCKER_CONTAINER_NAME" \
     --volumes-from "${DUMMY_DOCKER_CONTAINER_NAME}" \
     --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    /bin/bash -c "cd /app && nosetests --with-coverage --cover-branches --cover-package=$("${SCRIPT_DIR_NAME}/repo.sh" -u)"
+    /bin/bash -c "cd /app && nosetests --with-coverage --cover-branches --cover-package=$("${SCRIPT_DIR_NAME}/repo.sh" -u) && if [ -e /app/.coverage ]; then sed -i -e 's|/app/|$REPO_ROOT_DIR/|g' /app/.coverage; fi"
 
-REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
-DOT_COVERAGE=$REPO_ROOT_DIR/.coverage
-set -x
-docker container cp "$DOCKER_CONTAINER_NAME:/app/.coverage" "$DOT_COVERAGE"
-if [ -e "$DOT_COVERAGE" ]; then
-    ls -la "$DOT_COVERAGE"
-    sed -i '' -e "s|/app/|$REPO_ROOT_DIR/|g" "$DOT_COVERAGE"
-fi
-set +x
+docker container cp \
+    "$DOCKER_CONTAINER_NAME:/app/.coverage" \
+    "$DOT_COVERAGE"
 
 docker container rm "$DOCKER_CONTAINER_NAME" > /dev/null
 
