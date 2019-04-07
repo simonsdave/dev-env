af0fb92518220ae92637a09fb744b5202e4f01c2
Dave Simons <simonsdave@gmail.com>
2019-04-07 08:55:54 -0400

    refactor: simplify run-unit-tests.sh

--- bin/run-unit-tests.sh
+++ bin/run-unit-tests.sh
@@ -9,34 +9,26 @@ if [ $# != 0 ]; then
     exit 1
 fi
 
-REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
-REPO=$("${SCRIPT_DIR_NAME}/repo.sh" -u)
-DOT_COVERAGE=$REPO_ROOT_DIR/.coverage
-
-#
-# :TRICKY: nosetests is run inside the docker container.
-# and if we simply let this happen the container would
-# set the uid and gid of .coverage which can then make
-# .coverage inaccessible by the host. to solve this
-# problem we create an empty .coverage here (which runs
-# on the host) so we can control .coverage's uid and gid.
-#
-echo "!coverage.py: This is a private format, don't read it directly!{}" > "$DOT_COVERAGE"
-chmod a+wr "$DOT_COVERAGE"
+DOCKER_CONTAINER_NAME=$(python -c "import uuid; print uuid.uuid4().hex")
 
 DUMMY_DOCKER_CONTAINER_NAME=$("${SCRIPT_DIR_NAME}/create-dummy-docker-container.sh")
 
 docker run \
-    --rm \
-    --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
+    --name "$DOCKER_CONTAINER_NAME" \
     --volumes-from "${DUMMY_DOCKER_CONTAINER_NAME}" \
+    --security-opt "${DEV_ENV_SECURITY_OPT:-seccomp:unconfined}" \
     "$DEV_ENV_DOCKER_IMAGE" \
-    /bin/bash -c "cd /app && nosetests --with-coverage --cover-branches --cover-package=${REPO}"
+    /bin/bash -c "cd /app && nosetests --with-coverage --cover-branches --cover-package=$("${SCRIPT_DIR_NAME}/repo.sh" -u)"
 
-docker rm "${DUMMY_DOCKER_CONTAINER_NAME}" > /dev/null
-
+REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
+DOT_COVERAGE=$REPO_ROOT_DIR/.coverage
+docker container cp "$DOCKER_CONTAINER_NAME:/app/.coverage" "$DOT_COVERAGE"
 if [ -e "$DOT_COVERAGE" ]; then
     sed -i '' -e "s|/app/|$REPO_ROOT_DIR/|g" "$DOT_COVERAGE"
 fi
+
+docker container rm "$DOCKER_CONTAINER_NAME" > /dev/null
+
+docker rm "${DUMMY_DOCKER_CONTAINER_NAME}" > /dev/null
 
 exit 0
