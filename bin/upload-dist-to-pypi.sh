4a86544fa32bc75fbee8f707cb20df6ab1cedca2
Dave Simons <simonsdave@gmail.com>
2019-04-14 22:25:02 -0400

    fix: upload-dist-to-pypi.sh for dev-env itself

--- bin/upload-dist-to-pypi.sh
+++ bin/upload-dist-to-pypi.sh
@@ -12,22 +12,30 @@ fi
 REPO=${1:-}
 
 DOT_PYPIRC=$HOME/.pypirc
-if [ ! -e "$DOT_PYPIRC" ]; then
-    echo "Could not find '$DOT_PYPIRC'" >&2
+if [ ! -e "${DOT_PYPIRC}" ]; then
+    echo "Could not find '${DOT_PYPIRC}'" >&2
     exit 1
 fi
 
-DIST_DIRECTORY=$("$SCRIPT_DIR_NAME/repo-root-dir.sh")/dist
-if [ ! -d "$DIST_DIRECTORY" ]; then
-    echo "Could not find package directory '$DIST_DIRECTORY'" >&2
+REPO_ROOT_DIR=$("$SCRIPT_DIR_NAME/repo-root-dir.sh")
+DIST_DIR=${REPO_ROOT_DIR}/dist
+if [ ! -d "${DIST_DIR}" ]; then
+    echo "Could not find package directory '${DIST_DIR}'" >&2
     exit 1
 fi
 
+#
+# :TRICKY: little odd to explicitly address /usr/local/bin/upload-dist-to-pypi.sh
+# below rather than let the PATH sort out where this script is located. however,
+# with running this script for dev-env itself upload-dist-to-pypi.sh on the host
+# would have been run recursively because /app/bin was the first directory in
+# the docker container's path:-(
+#
 docker run \
     --rm \
-    --volume "$("$SCRIPT_DIR_NAME/repo-root-dir.sh"):/app" \
-    --volume "$HOME:/pypirc" \
-    "$DEV_ENV_DOCKER_IMAGE" \
-    upload-dist-to-pypi.sh "$REPO"
+    --volume "${REPO_ROOT_DIR}:/app" \
+    --volume "${HOME}:/pypirc" \
+    "${DEV_ENV_DOCKER_IMAGE}" \
+    /usr/local/bin/upload-dist-to-pypi.sh "${REPO}"
 
 exit 0
