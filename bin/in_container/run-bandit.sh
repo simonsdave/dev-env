2ecfca7a00427c64bb45191fd1d722b3f8941fd5
Dave Simons <simonsdave@gmail.com>
2019-04-17 21:42:44 -0400

    chore: should have been part of 11b24436e878f3ed

---
+++ bin/in_container/run-bandit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+REPO_ROOT_DIR=$("$SCRIPT_DIR_NAME/repo-root-dir.sh")
+PACKAGE=$("$SCRIPT_DIR_NAME/repo.sh")
+
+bandit --verbose --exclude "${REPO_ROOT_DIR}/env" -r "${REPO_ROOT_DIR}/${PACKAGE}"
+
+exit 0
---
+++ bin/run-bandit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+DUMMY_DOCKER_CONTAINER_NAME=$("${SCRIPT_DIR_NAME}/create-dummy-docker-container.sh")
+
+docker run \
+    --rm \
+    --volumes-from "${DUMMY_DOCKER_CONTAINER_NAME}" \
+    "$DEV_ENV_DOCKER_IMAGE" \
+    /bin/bash -c "bandit --verbose --exclude '/app/env' -r '/app/$("$SCRIPT_DIR_NAME/repo.sh" -u)'"
+
+docker rm "${DUMMY_DOCKER_CONTAINER_NAME}" > /dev/null
+
+exit 0
