d55c139bbf53a4c10d5e36ffb9400a57c9774f85
Dave Simons <simonsdave@gmail.com>
2019-05-05 20:08:32 -0400

    feat: v1 of bin/increment_version.sh

---
+++ bin/install-dev-env.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# curl -s -L https://raw.githubusercontent.com/simonsdave/dev-env/master/bin/install-dev-env.sh | bash -s --
+
+set -e
+
+if [ $# != 0 ]; then
+    echo "usage: $(basename "$0")" >&2
+    exit 1
+fi
+
+REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
+DEV_ENV_VERSION=$(cat "${REPO_ROOT_DIR}/dev_env/dev-env-version.txt")
+if [ "${DEV_ENV_VERSION:-}" == "latest" ]; then DEV_ENV_VERSION=master; fi
+
+pip install "git+https://github.com/simonsdave/dev-env.git@$DEV_ENV_VERSION"
+
+REPO_DOT_SH_DIR=$(dirname $(which repo.sh))
+INCREMENT_VERSION_DOT_SH=${REPO_DOT_SH_DIR}/increment_version.sh
+curl -s -L -o "${INCREMENT_VERSION_DOT_SH}" "https://raw.githubusercontent.com/fmahnke/shell-semver/master/increment_version.sh"
+chmod a+x "${INCREMENT_VERSION_DOT_SH}"
+
+exit 0
