44c1084862a51261d977c73b01bf0654bd8151ff
Dave Simons <simonsdave@gmail.com>
2019-12-15 17:58:16 -0500

    feat: convert .cut-release-master-branch-changes.sh for python 3.7

--- .cut-release-master-branch-changes.sh
+++ .cut-release-master-branch-changes.sh
@@ -17,7 +17,7 @@ pip install semantic-version > /dev/null
 
 CURRENT_VERSION=$(python-version.sh)
 
-NEXT_VERSION=$(python -c "import semantic_version; print semantic_version.Version('$CURRENT_VERSION').next_patch()")
+NEXT_VERSION=$(python -c "import semantic_version; print(semantic_version.Version('$CURRENT_VERSION').next_patch())")
 
 INIT_DOT_PY=$(repo-root-dir.sh)/$(repo.sh -u)/__init__.py
 sed -i '' -e "s|^[[:space:]]*__version__[[:space:]]*=[[:space:]]*['\"]${CURRENT_VERSION}['\"][[:space:]]*$|__version__ = '${NEXT_VERSION}'|g" "${INIT_DOT_PY}"
