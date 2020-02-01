9025f6b2230f52cecae0578a141142a4dcfba171
Dave Simons <simonsdave@gmail.com>
2020-01-31 21:45:42 -0500

    debug: is it ever going to end:-(

--- bin/build-readme-dot-rst.sh
+++ bin/build-readme-dot-rst.sh
@@ -49,6 +49,7 @@ echo "${0} - 222222222222222222222222222222222"
 
 echo ">>>>>>>>>>>>>${PWD}<<<<<<<<<<<<<<<<<"
 echo ">>>>>>>>>>>>>${SCRIPT_DIR_NAME}<<<<<<<<<<<<<<<<<"
+set -x
 REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
 
 DOCKER_CONTAINER_NAME=$(openssl rand -hex 16)
