ba58915e0a0fb26e46be7c475e1e90158f835bdb
Dave Simons <simonsdave@gmail.com>
2022-01-02 12:12:08 -0500

    refactor: run-codecov-uploader.sh in right pattern

--- bin/run-codecov-uploader.sh
+++ bin/run-codecov-uploader.sh
@@ -9,15 +9,28 @@ set -e
 
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ $# != 0 ]; then
-    echo "usage: $(basename "$0")" >&2
-    exit 1
+#
+# :TRICKY: want to use this script to run for app repos
+# as well as the dev-env repo. All works for app repos
+# but with dev-env the "docker run" below runs run-markdownlint.sh
+# and since the dev-env docker build puts /app/bin
+# at the start of the PATH, the docker run will actually
+# run this script instead of the intended script in the
+# in-container directory.
+#
+if [[ "/app/bin/${0##*/}" == "${0}" ]]; then
+    "${SCRIPT_DIR_NAME}/in-container/${0##*/}" "$@"
+    exit $?
 fi
 
-REPO_ROOT_DIR=$("${SCRIPT_DIR_NAME}/repo-root-dir.sh")
-DOT_COVERAGE=${REPO_ROOT_DIR}/.coverage
-if [ -e "${DOT_COVERAGE}" ]; then
-    codecov -f "${DOT_COVERAGE}"
-fi
+DUMMY_DOCKER_CONTAINER_NAME=$("${SCRIPT_DIR_NAME}/create-dummy-docker-container.sh")
+
+docker run \
+    --rm \
+    --volumes-from "${DUMMY_DOCKER_CONTAINER_NAME}" \
+    "${DEV_ENV_DOCKER_IMAGE}" \
+    "${0##*/}" "$@"
+
+docker rm "${DUMMY_DOCKER_CONTAINER_NAME}" > /dev/null
 
 exit 0
